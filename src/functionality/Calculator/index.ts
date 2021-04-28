d6e48d34c650cbaf43739d41e8dfb188ea36d686
Your Name <you@example.com>
2021-04-27 23:36:14 -0400

    logic Abnormality in return value

--- src/functionality/Calculator/index.ts
+++ src/functionality/Calculator/index.ts
@@ -14,13 +14,14 @@ const Index = (expression: string): number | string => {
 
   //Split the expression into an array and remove the white space 
   const expressionSplitArray = expression.split('').filter(nonWhiteSpaceElement => { return nonWhiteSpaceElement != ' ' });
-  console.log(expressionSplitArray)
+
   //Validatied array 
   const validatedArray: string[] = validateInput(expressionSplitArray);
-  //console.log(validatedArray);
+
 
   //if the array returned is one element an error occured, return the answer is the error message else calculate!
-  answer = (validatedArray[0] !== "Syntax Error" || "Invalid Input") ? calculate(validatedArray) : validatedArray[0]
+  if (validatedArray.length === 1 && (validatedArray[0] === "Syntax Error" || "Invalid Input")) { answer = validatedArray[0] }
+  else { answer = calculate(validatedArray) }
   return answer
 }
 
